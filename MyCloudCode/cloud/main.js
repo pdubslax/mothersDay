e71e90fb6f3907e33fe816a36c4ba17dfd1cbaf9
Patrick Wilson <pdubslax@umich.edu>
2015-05-09 22:54:01 -0400

    update push

---
+++ MyCloudCode/cloud/main.js
@@ -0,0 +1,28 @@
+
+// Use Parse.Cloud.define to define as many cloud functions as you want.
+// For example:
+Parse.Cloud.define("hello", function(request, response) {
+  response.success("Hello world!");
+});
+
+
+
+
+Parse.Cloud.job("pushUsers", function(request, status) {
+  // Set up to modify user data
+  var query = new Parse.Query(Parse.Installation);
+
+	Parse.Push.send({
+	  where: query, // Set our Installation query
+	  data: {
+	    alert: "Hello Mother! Thank you for everything you have done. Another note is ready for you now!"
+	  }
+	}, {
+	  success: function() {
+	    // Push was successful
+	  },
+	  error: function(error) {
+	    // Handle error
+	  }
+	});
+});
